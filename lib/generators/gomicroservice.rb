91fa58d5a96da1ed1ceb08680a076bf53a0d6485
Nic Jackson <jackson.nic@gmail.com>
2016-05-20 13:28:50 +0100

    updated gitignore

--- lib/generators/gomicroservice.rb
+++ lib/generators/gomicroservice.rb
@@ -12,16 +12,18 @@ module Minke
       config.template_location = File.expand_path(File.dirname(__FILE__)) + '/gomicroservice/scaffold'
 
       config.build_settings = Minke::Generators::BuildSettings.new
+
       config.build_settings.build_commands = Minke::Generators::BuildCommands.new.tap do |bc|
         bc.fetch = [['/bin/bash', '-c', 'go get -t -v -d ./... && echo Downloaded packages']]
         bc.build = [['/bin/bash', '-c', 'go build -a -installsuffix cgo -ldflags \'-s\' -o <%= application_name %>']]
         bc.test = [['/bin/bash', '-c', 'go test ./...']]
       end
+
       config.build_settings.docker_settings = Minke::Generators::DockerSettings.new.tap do |bs|
         bs.image = 'golang:latest'
         bs.env = ['CGO_ENABLED=0']
         bs.binds = ["#{ENV['GOPATH']}/src:/go/src"]
-        #bs.working_directory = 'sdsds'#{}"#{self.working_directory}"
+        # working directory is added via a class patch to enable lazy load
       end
 
       # we need lazy evaluation of this property
